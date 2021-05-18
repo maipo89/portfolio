0a812921e67fccacf7d1c9d3b583feebbeddbffa
maipo89 <67272086+maipo89@users.noreply.github.com>
2021-05-18 23:08:12 +0200

    Work Section ternary operator

--- src/components/data.js
+++ src/components/data.js
@@ -3,6 +3,9 @@ import easyBankMobile from "../images/easy-bank-mobile.jpg"
 import tuttoPepe from "../images/tutto-pepe-landing-page.jpg"
 import tuttoPepeSecondPage from "../images/tutto-pepe-second-page.jpg"
 import tuttoPepeMobile from "../images/tutto-pepe-mobile.jpg"
+import portfolio from "../images/portfolio.jpg"
+import portfolioAbout from "../images/portfolio-about.jpg"
+import portfolioPage from "../images/portfolio-portfolio.jpg"
 
 const dataPortfolio = [
     {
@@ -16,20 +19,22 @@ const dataPortfolio = [
         technologies: ["HTML", "CSS", "JavaScript", "SVG"],
         link: "https://maipo89.github.io/easybank-landingpage/",
         year: "2021",
-        id: 2
+        id: 1
     },
     {
         title: "Marco Brunetti Portfolio Website", 
         subtitle: "This is my portfolio, an application built from scratch with React (using React-routing and Hooks). I did the UI design and the style using and deployed with Heroku",
         imgs: [
             {title: "Desktop Version", 
-             img: easyBank},
-            {title: "Mobile Version",
-             img: easyBankMobile}],
+             img: portfolio},
+            {title: "About Animation",
+             img: portfolioAbout},
+            {title: "Portfolio Page",
+             img: portfolioPage}],
         technologies: ["HTML", "CSS", "React", "React-Router"],
         link: "https://www.marcobrunetti.com",
         year: "2021",
-        id: 1
+        id: 2
     },
     {
     title: "Tutto Pepe front-end E-Commerce", 
